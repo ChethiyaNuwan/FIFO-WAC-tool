a2486bf46547ead09d87609f69f66dee2c480c1b
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2022-10-25 14:08:32 +0530

    improved method switch function

--- testJS.js
+++ testJS.js
@@ -5,12 +5,14 @@ function methodSwitch(){
     if (fifowacState == 'w') {
         document.getElementById('balUP').style.display = "none";
         document.getElementById('tableCap').innerHTML = "Inventory System on FIFO";
+        document.getElementById('fifowac').value = "Switch to WAC";
         fifowacState = 'f';
     }
 
     else if (fifowacState == 'f') {
         document.getElementById('balUP').style.display = "";
         document.getElementById('tableCap').innerHTML = "Inventory System on WAC";
+        document.getElementById('fifowac').value = "Switch to FIFO";
         fifowacState = 'w';
     }
 }
