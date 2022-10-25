081cd487fec4cd3e49a3c70ab55293f3786a180c
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2022-10-26 02:54:24 +0530

    added php code forDB connect

--- Main.php
+++ Main.php
@@ -1,3 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "smashAudit";
+$password = "smashAudit";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
