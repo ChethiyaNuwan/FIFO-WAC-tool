933dbb0f3b9380fea824b5b1a027976564e3e3f9
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2022-10-25 15:41:04 +0530

    finalized method switch function

--- testJS.js
+++ testJS.js
@@ -2,17 +2,27 @@ var fifowacState = 'w';
 var rowNumber = 0;
 
 function methodSwitch(){
+    var balUPs=document.getElementsByClassName('balUP');
+
     if (fifowacState == 'w') {
-        document.getElementById('balUP').style.display = "none";
+        for (let i = 0; i < balUPs.length; i++) {
+            balUPs[i].style.display="none";
+        }
+
         document.getElementById('tableCap').innerHTML = "Inventory System on FIFO";
         document.getElementById('fifowac').value = "Switch to WAC";
+        document.getElementById('balance').colSpan="2";
         fifowacState = 'f';
     }
 
     else if (fifowacState == 'f') {
-        document.getElementById('balUP').style.display = "";
+        for (let i = 0; i < balUPs.length; i++) {
+            balUPs[i].style.display="";
+        }
+
         document.getElementById('tableCap').innerHTML = "Inventory System on WAC";
         document.getElementById('fifowac').value = "Switch to FIFO";
+        document.getElementById('balance').colSpan="3";
         fifowacState = 'w';
     }
 }
